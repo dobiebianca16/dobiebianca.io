b0b4897a2879f709501aae6a98bd18cde6b98106
Dobie Bianca <72404684+dobiebianca16@users.noreply.github.com>
2022-11-23 22:44:20 +0200

    setup

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
